b22ec595c6ffab2fe3c1d9a0bffea51902d0e970
casav929 <casav929@student.liu.se>
2025-09-09 08:15:52 +0200

    Added flutter-tools plugin

--- lua/plugins/plugins.lua
+++ lua/plugins/plugins.lua
@@ -13,4 +13,14 @@ return {
       })
     end,
   },
+
+  {
+    "nvim-flutter/flutter-tools.nvim",
+    lazy = false,
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "stevearc/dressing.nvim", -- optional for vim.ui.select
+    },
+    config = true,
+  },
 }
