bcdf636ec4cc3f820059af89e164eadb41f240fa
casav929 <casav929@student.liu.se>
2026-04-03 09:40:16 +0200

    rm instant

--- lua/plugins/instant.lua
+++
@@ -1,7 +0,0 @@
-return {
-  "jbyuki/instant.nvim",
-  lazy = false, -- gör så att pluginet laddas direkt
-  config = function()
-    vim.g.instant_username = "Casper" -- byt till ditt namn eller alias
-  end,
-}
